23293a4b98185c6828dd4c2b7b6b77b94a4ca916
Danielle Luz <daniln2209@gmail.com>
2024-06-08 14:07:59 -0300

    feat: created read lines method

--- src/services/FileReader.java
+++ src/services/FileReader.java
@@ -1,3 +1,31 @@
 package services;
 
-public class FileReader {}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FileReader {
+
+  public static Set<String> readUniqueLinesFromFile(String fileName) {
+    String filePath = Paths.get("../resources/input", fileName).toString();
+    Set<String> fileLines = new HashSet<>();
+
+    try (
+      BufferedReader reader = new BufferedReader(
+        new java.io.FileReader(filePath)
+      )
+    ) {
+      String line = reader.readLine();
+
+      while (line != null) {
+        fileLines.add(line);
+      }
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+
+    return fileLines;
+  }
+}
